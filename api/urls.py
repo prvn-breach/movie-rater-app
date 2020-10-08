a74cadbcc7940afa7bae5d70c4af95a6406db475
prvn-breach <praveenking880@gmail.com>
2020-10-08 17:11:34 +0530

    changes

---
+++ api/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import include
+from rest_framework import routers
+from api.views import MovieViewSet,RatingViewSet,UserViewSet
+
+router = routers.DefaultRouter()
+router.register('users', UserViewSet)
+router.register('movies', MovieViewSet)
+router.register('ratings', RatingViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
